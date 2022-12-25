2a65944e9e61bc334ee723f73d0974595a49ad40
Arturo Gallegos <lgallegos@ia.com.mx>
2022-12-25 01:57:15 -0600

    Feat: Enable mongodb

---
+++ database/db.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+let dbIsConnected = 0;
+const connect = async () => {
+  if(dbIsConnected === 1) return;
+
+  const connections = mongoose.connections;
+
+  if(connections.length > 0) {
+
+    dbIsConnected = connections[0].readyState;
+
+    if(dbIsConnected === 1) return;
+
+    await mongoose.disconnect();
+    dbIsConnected = 0;
+  };
+
+  await mongoose.connect(process.env.MONGO_DB || '')
+  dbIsConnected = 1
+}
+
+const disconnect = () => {
+  if(dbIsConnected === 0 || process.env.NODE_ENV === 'development') return;
+
+  mongoose.disconnect();
+  dbIsConnected = 0;
+}
+
+const db = {
+  connect,
+  disconnect,
+};
+
+export default db;
---
+++ fake-data/products.ts
@@ -0,0 +1,10 @@
+
+const products = [
+  {
+    title: 'product 1',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, voluptatem!',
+    price: 12
+  },
+];
+
+export default products;
